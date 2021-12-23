0a11f606445a1227909103c50137a3d40650e2b8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-12-23 14:34:18 +0530

    Update main.ts

--- bug-tracker-app/src/main.ts
+++ bug-tracker-app/src/main.ts
@@ -3,9 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+/* 
 import * as moment from 'moment';
-console.log(moment('2021-12-23T07:13:56.390Z').fromNow())
+console.log(moment('2021-12-23T07:13:56.390Z').fromNow()) 
+*/
 
 if (environment.production) {
   enableProdMode();
